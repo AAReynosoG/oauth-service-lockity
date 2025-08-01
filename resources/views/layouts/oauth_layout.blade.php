a6b7c8ff1f3a1318380904e6d11438194ec86a98
Alonso <abrahamreynoso64@gmail.com>
2025-08-01 10:13:16 -0600

    feat: agregando favicon

--- resources/views/layouts/oauth_layout.blade.php
+++ resources/views/layouts/oauth_layout.blade.php
@@ -6,6 +6,8 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="icon" type="image/png" href="https://guikspbicskovcmvfvwb.supabase.co/storage/v1/object/public/lockity-public-images/logos/lockity-logo-no-title.png">
+
     <style>
         * {
             box-sizing: border-box;
--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autorización - Lockity</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="https://guikspbicskovcmvfvwb.supabase.co/storage/v1/object/public/lockity-public-images/logos/lockity-logo-no-title.png">
     <style>
         body {
             background-color: #2E2D2D;
